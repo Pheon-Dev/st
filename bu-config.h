0112993cd8f52dfe01fd8e542fa2f943cc3ea20d
Pheon-Dev <devpheon@gmail.com>
2022-11-16 18:46:35 +0300

    patch: cursor blink

--- bu-config.h
+++ bu-config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Noto Sans Mono:size=8";
+static char *font = "Noto Sans Mono:size=9";
 static int borderpx = 0;
 
 /*
--- config.h
+++ config.h
@@ -172,8 +172,21 @@ unsigned int defaultunderline = 7;
  * 4: Underline ("_")
  * 6: Bar ("|")
  * 7: Snowman ("☃")
+ * https://invisible-island.net/xterm/ctlseqs/ctlseqs.html#h4-Functions-using-CSI-_-ordered-by-the-final-character-lparen-s-rparen:CSI-Ps-SP-q.1D81
+ * Default style of cursor
+ * 0: blinking block
+ * 1: blinking block (default)
+ * 2: steady block ("â–ˆ")
+ * 3: blinking underline
+ * 4: steady underline ("_")
+ * 5: blinking bar
+ * 6: steady bar ("|")
+ * 7: blinking st cursor
+ * 8: steady st cursor
  */
-static unsigned int cursorshape = 6;
+// static unsigned int cursorshape = 6;
+static unsigned int cursorstyle = 1;
+static Rune stcursor = 0x2603; /* snowman ("â˜ƒ") */
 
 /*
  * Default columns and rows numbers
