74ee891503addf29a2610e21505ae901c3e91e68
Pheon-Dev <devpheon@gmail.com>
2024-05-03 11:07:56 +0300

    [~] character spacing

--- config.h
+++ config.h
@@ -32,7 +32,7 @@ char *stty_args = "stty raw pass8 nl -echo -iexten -cstopb 38400";
 char *vtiden = "\033[?6c";
 
 /* Kerning / character bounding-box multipliers */
-static float cwscale = 0.90;
+static float cwscale = 0.85;
 static float chscale = 1.0;
 
 /* Character rendering offsets in pixels */
